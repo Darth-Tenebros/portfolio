ee14b3719a84f0832f35062d3b97c99d0c827a19
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-11 16:19:00 +0200

    context

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using portfolio.Models;
+
+namespace portfolio.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Project> Projects {get; set;}
+    }
+}
