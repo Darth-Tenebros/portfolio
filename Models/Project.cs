aad4db3d2b0293672687a2f219dd1ab78c98d9e4
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-11 16:18:36 +0200

    db model

---
+++ Models/Project.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portfolio.Models
+{
+    public class Project
+    {
+        public int Id {get; set;}
+        public string? name {get; set;}
+        public string? html_url {get; set;}
+        public DateTime created_at {get; set;}
+        public DateTime updated_at {get; set;}
+        public string? language {get;set;}
+    }
+}
