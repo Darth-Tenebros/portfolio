2febecd61e7f48bf20672e456ce7abe525c1745b
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-11 16:19:55 +0200

    project interface

---
+++ Interfaces/IProjectsRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using portfolio.Models;
+
+namespace portfolio.Interfaces
+{
+    public interface IProjectsRepository
+    {
+        bool ProjectsExists(int id);
+        ICollection<Project> GetProjects();
+        bool UpdateProject(Project project);
+        bool CreateProject(Project project);
+        bool Save();
+    }
+}
