1118b13aec6fdd2feffa86c98c323fa8ce8d2e62
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-12 13:16:14 +0200

    impl github service

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using portfolio.Github;
+using portfolio.Models;
 
 namespace portfolio.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public ICollection<Project> Repos { get; set; } = new List<Project>();
     public string? Name {get; set;}
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        
     }
 
     public void OnGet()
     {
-        Name = "yolisa pingilili";
+        Repos = new GithubService().FetchAndDisplayRepos("darth-tenebros");
     }
 }
