52a1d3a91774b65bc6d2fa12fe28ed0f83e87501
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-16 23:33:45 +0200

    inject db repo dependency

--- Pages/Projects.cshtml.cs
+++ Pages/Projects.cshtml.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using portfolio.Interfaces;
+using portfolio.Models;
 
 namespace portfolio.Pages;
 
 public class PrivacyModel : PageModel
 {
     private readonly ILogger<PrivacyModel> _logger;
+    private IProjectsRepository _projectRepossitory;
+    public ICollection<Project> projects {get; set;}
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public PrivacyModel(ILogger<PrivacyModel> logger, IProjectsRepository projectsRepository)
     {
         _logger = logger;
+        _projectRepossitory = projectsRepository;
     }
 
     public void OnGet()
     {
+        projects = _projectRepossitory.GetProjects();
     }
 }
 
