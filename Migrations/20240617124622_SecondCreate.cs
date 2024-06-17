c3139ecd9fddb75181dc0c020b945711ab9cbefb
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-17 15:52:55 +0200

    new db migrations

---
+++ Migrations/20240617124622_SecondCreate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace portfolio.Migrations
+{
+    public partial class SecondCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "Projects");
+        }
+    }
+}
