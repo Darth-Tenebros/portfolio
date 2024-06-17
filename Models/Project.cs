358605f3f34c720041fc319a08d75ff5b0d30919
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-06-17 15:54:10 +0200

    add another field to the model

--- Models/Project.cs
+++ Models/Project.cs
@@ -10,6 +10,7 @@ namespace portfolio.Models
         public int Id {get; set;}
         public string? name {get; set;}
         public string? html_url {get; set;}
+        public string? description {get; set;}
         public DateTime created_at {get; set;}
         public DateTime updated_at {get; set;}
         public string? language {get;set;}
