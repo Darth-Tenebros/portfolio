59ce696ab42656006fd2d05884ea683e4da06fdc
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-07-04 17:02:12 +0200

    require auth on route

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,6 +11,7 @@ using portfolio.Models;
 
 namespace portfolio.Pages
 {
+    [Authorize]
     public class Dashboard : PageModel
     {
         private readonly ILogger<Dashboard> _logger;
