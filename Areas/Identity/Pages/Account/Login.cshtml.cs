f0c0eb543300b4f4c240caa3767d17b8efb2c710
Darth-Tenebros <yolisapingilili2@gmail.com>
2024-07-06 20:55:06 +0200

    add support for username

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -83,6 +83,9 @@ namespace portfolio.Areas.Identity.Pages.Account
             /// </summary>
             [Display(Name = "Remember me?")]
             public bool RememberMe { get; set; }
+
+            [Required]
+            public string UserName { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -112,7 +115,7 @@ namespace portfolio.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
